c134a7254b308d79d7cab5ad6994d10f9938cf2f
B Stack <bgstack15@gmail.com>
2017-11-12 10:00:47 -0500

    add pwhashgen.py

---
+++ usr/share/bgscripts/py/pwhashgen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# File: /usr/share/bgscripts/py/pwhashgen.py
+# Author: bgstack15@gmail.com
+# Startdate: 2017-11-06
+# Title: Python Script that Generates Password Hashes for /etc/shadow
+# Purpose: See title.
+# History:
+# Usage: ./pwhashgen.py 'plaintextpw'
+#    Or ./pwhashgen.py
+#    and let it prompt you for a new password
+# Reference: https://www.shellhacks.com/linux-generate-password-hash/
+# Improve: Make work with python3 and python2
+# Document: below this line
+import crypt, getpass, sys;
+if len(sys.argv) >= 2:
+   thisraw=str(sys.argv[1]);
+else:
+   thisraw=getpass.getpass(prompt='New password: ')
+   #sys.exit(1)
+print(crypt.crypt(thisraw,crypt.mksalt(crypt.METHOD_SHA512)))
+
